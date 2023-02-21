cd72a8db8eb7938b8ca6442c116ec46b4553f48c
penpizza <penpicha@gmail.com>
2023-02-21 16:42:25 +0700

    completed

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,22 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1,Rect *R2){
+	double ax2 = R1->x + R1->w;
+	double bx2 = R2->x + R2->w;
+	double ax1 = R1->x;
+	double bx1 = R2->x;
 
+	double ay2 = R1->y - R1->h;
+	double by2 = R2->y - R2->h;
+	double ay1 = R1->y;
+	double by1 = R2->y;
 
-
+	double lw = min(ax2,bx2) - max(ax1,bx1);
+	double lh = min(ay1,by1) - max(ay2,by2);
+	if (lw > 0){
+		return lw*lh;
+	}else  {
+	return 0;
+	}
 }
